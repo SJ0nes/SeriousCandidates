90450adddde570a75333885c96cd0b6885f865a2
TomMcGarel <165075874+TomMcGarel@users.noreply.github.com>
2024-03-26 18:56:25 +0000

    Add files via upload

---
+++ QuanTom.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+import sympy as sym 
+
+import matplotlib.pyplot as plt
+
+
+
+def calculate_wave_speed(wavelength, frequency):
+    """
+    Wavespeed is the distance travelled by a wave per unit time.
+
+    The equation returns the speed of a wave in metres per second.
+
+    It takes wavelength (in metres) and frequency (in hertz) as inputs.
+    """ 
+    return wavelength * frequency
+
+git config --global user.name "TomMcGarel"
+git config --global user.email "tommcgarel@gmail.com"
