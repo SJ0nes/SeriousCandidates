b36d3151f5540ef22ebb9bc24f825eb73439a064
Sam <58917078+SJ0nes@users.noreply.github.com>
2024-05-02 19:10:05 +0100

    Update library.py

--- library.py
+++ library.py
@@ -1,73 +1,67 @@
-# defining all the necessary variables
 wavelength:float = None
 time:float = None
 frequency:float = None
 speed_of_light = 3e8
 planks_constant: float = 6.626e-34
-mass:float = 9.11e-31
+mass:float = 9.11e-31 
 
-def do_calculate_wave_speed_by_frequency(frequency): 
+def do_calculate_wave_speed_by_frequency(wavelength, frequency):
     return wavelength * frequency
 
-def do_calculate_wave_speed_by_time(time):
-    if time == 0: #since dividing by time, ensuring there isn't any errors
-        return None 
-    else:
+def do_calculate_wave_speed_by_time(wavelength, time):
+    if time > 0: #time cannot be negative
         return wavelength / time
+    else:
+        return None
 
-def do_calculate_de_broglie_by_momentum(momentum):
+def do_calculate_de_broglie_by_momentum(momentum): 
     if momentum == 0:
         return None # preventing division by 0
     else:
         return (planks_constant / momentum)
         
-def do_calculate_de_broglie_by_velocity(velocity): 
+def do_calculate_de_broglie_by_velocity(velocity):
     if velocity == 0: # preventing division by 0
         return None
     else:
         return (planks_constant / (mass * velocity))
 
 def do_calculate_photon_energy_by_time(time):
-    if time == 0: #preventing division by 0
-        return None
-    else:
+    if time>0: #preventing division by 0 and restricting time to be positive
         return (planks_constant / time)
+    else:
+        return None
         
 def do_calculate_photon_energy_by_frequency(frequency): 
-        return (planks_constant * frequency) 
+    return (planks_constant * frequency) 
 
 def do_calculate_photon_energy_by_wavelength(wavelength): 
     if wavelength == 0: # preventing division by 0
-        return None #none is the null value
+        return None 
     else:
         return ((planks_constant * speed_of_light) / wavelength)     
         
 
-#the functions used in practice while using the library
-def calculate_wave_speed(wavelength:float = None, frequency:float = None, time:float = None) -> float: # wavespeed = wavelength * frequency    
+
+def calculate_wave_speed(wavelength:float = None, frequency:float = None, time:float = None) -> float: 
     if wavelength != None and frequency == None:
-        return do_calculate_wave_speed_by_time(time)
-    if wavelength != None and time == None:
-        return do_calculate_wave_speed_by_frequency(frequency)
+        return do_calculate_wave_speed_by_time(wavelength, time)
+    elif wavelength != None and time == None:
+        return do_calculate_wave_speed_by_frequency(wavelength, frequency)
     else:
         return None
-  
-def calculate_photon_energy(frequency:float=None, time:float=None, wavelength:float=None) -> float: 
-    if frequency==None and time==None and wavelength==None: #ensuring only 1 parameter is entered.
-        return None
-    if frequency!=None and time!=None and wavelength!=None:        
-        return None
-    
-    if frequency==None and time==None: # using wavelenth
+   
+def calculate_photon_energy(frequency:float=None, time:float=None, wavelength:float=None) -> float:
+    if frequency==None and time==None:  
         return do_calculate_photon_energy_by_wavelength(wavelength)
-    if time==None and wavelength==None: # using frequency
+    elif time==None and wavelength==None:
         return do_calculate_photon_energy_by_frequency(frequency)
-    if frequency==None and wavelength==None: # using time
+    elif frequency==None and wavelength==None:    
         return do_calculate_photon_energy_by_time(time)
     else:
         return None
 
-def calculate_photoelectric_effect(workfunction:float, kinetic_energy:float) -> float:
+def calculate_photoelectric_effect(workfunction:float = None, kinetic_energy:float = None) -> float:
     return (workfunction + kinetic_energy)
 
 def calculate_kinetic_energy(velocity:float) -> float:
@@ -76,8 +70,8 @@ def calculate_kinetic_energy(velocity:float) -> float:
 def calculate_de_broglie(momentum:float = None, velocity:float = None, ) -> float: # 2 different formulas for calculating wavelength
     if momentum == None and velocity != None: # case where using velocity
         return do_calculate_de_broglie_by_velocity(velocity)
-    if momentum != None and velocity == None: # case where using momentum
+    elif momentum != None and velocity == None: # case where using momentum
         return do_calculate_de_broglie_by_momentum(momentum)
-    else: # ensuring no errors can occur if too many parameters are entered
+    else: # ensuring no errors can occur
         return None
     
